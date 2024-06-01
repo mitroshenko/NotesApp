55323f991487a432d900b2a8ce8eebe1ac48e69b
mitroshenko.ivan <mitroshenko.ivan@gmail.com>
2024-06-01 12:39:05 +0300

    Second commit

--- app/src/main/java/com/mitroshenko/mynotes/MainActivity.kt
+++ app/src/main/java/com/mitroshenko/mynotes/MainActivity.kt
@@ -3,7 +3,9 @@ package com.mitroshenko.mynotes
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.EditText
 import com.mitroshenko.mynotes.db.MyDbManager
+import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
     //Создаем манагера
@@ -12,11 +14,25 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
     }
 
     fun onClickSave(view: View) {
+        val twTest = findViewById<EditText>(R.id.twTest)
+        twTest.text = ""
         myDbManager.openDb()
-        myDbManager.insertToDb()
+        val edTitle = findViewById<EditText>(R.id.edTitle)
+        val edContent = findViewById<EditText>(R.id.edContent)
+        myDbManager.insertToDb(edTitle.text.toString(),edContent.text.toString())
+        val dataList = myDbManager.readDbData()
+        for (item  in dataList) {
+            twTest.append(item)
+            twTest.append("\n")
+
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        myDbManager.closeDb()
     }
 }
