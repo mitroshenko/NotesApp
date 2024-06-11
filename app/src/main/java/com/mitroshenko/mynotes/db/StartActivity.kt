ff659e096336b8d0c82c8fd162b3db0684074dcb
Dmitriy Yakovlev <y2kov.d@gmail.com>
2024-06-11 10:59:20 +0300

    Fix handler

--- app/src/main/java/com/mitroshenko/mynotes/db/StartActivity.kt
+++ app/src/main/java/com/mitroshenko/mynotes/db/StartActivity.kt
@@ -2,24 +2,24 @@ package com.mitroshenko.mynotes.db
 
 import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
 import androidx.appcompat.app.AppCompatActivity
 import com.mitroshenko.mynotes.MainActivity
 import com.mitroshenko.mynotes.R
-import java.util.Timer
-import java.util.logging.Handler
-import kotlin.concurrent.timerTask
 
 
 class StartActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_start)
-    }
-    val handler = Handler()
 
-    val runnable = Runnable {
+    private val handler = Handler()
+    private val runnable = Runnable {
         val i = Intent(this@StartActivity, MainActivity::class.java)
         startActivity(i)
     }
-    handler.postDelayed(Runnable, 4000)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_start)
+
+        handler.postDelayed(runnable, 4000)
+    }
 }
